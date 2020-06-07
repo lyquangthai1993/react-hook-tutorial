babbec4cc395362c38957a5fa7d1d94aba274469
Lý Quang Thái <lyquangthai1993@gmail.com>
2020-06-07 17:47:17 +0700

    edit text

--- src/screens/Home/home.js
+++ src/screens/Home/home.js
@@ -8,30 +8,43 @@ import TextBox from "components/TextBox";
 
 const TestSchema = yup.object().shape({
   name: yup.string().required("Name is required"),
+  favor: yup.string().required("Favor is required"),
 });
 
 const Home = (props) => {
   const {register, handleSubmit, errors} = useForm({
-    defaultValues: {name: ""},
+    defaultValues: {name: "", favor: ""},
     validationSchema: TestSchema,
   });
 
   const onSubmit = (data) => {
-    const {name} = data;
+    const {name, favor} = data;
     setTextState(name);
+    setFavorState(favor);
   };
 
   const [textState, setTextState] = useState("");
+  const [favorState, setFavorState] = useState("");
   return (
     <div className={"home-wrapper"}>
       <div className="text-center">
-        <h2>Hello '{textState}'</h2>
-        <h3>React hooks example: </h3>
+        <h2>
+          Hello '{textState}', and you like '{favorState}'
+        </h2>
+        <h3>React hooks form example: </h3>
       </div>
       {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
       <div className="container">
         <form onSubmit={handleSubmit(onSubmit)}>
           <TextBox label="Name" type="text" placeholder="Your name" name={"name"} register={register} errors={errors} />
+          <TextBox
+            label="Favor"
+            type="text"
+            placeholder="Your favor"
+            name={"favor"}
+            register={register}
+            errors={errors}
+          />
           <Button
             color="primary"
             type="submit"
@@ -39,7 +52,7 @@ const Home = (props) => {
               setTextState("");
             }}
           >
-            Set text
+            Submit
           </Button>
         </form>
       </div>
